09fd2ecf4fded9492018a84e512323e1587d7fbc
psa140 <psa140@sfu.ca>
2026-07-22 22:28:35 -0700

    test5

--- astro.config.ts
+++ astro.config.ts
@@ -13,6 +13,9 @@ import astroExpressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://allyftw.github.io",
+  base: "/intelliaudit/",
+  
   vite: {
     plugins: [tailwindcss()],
   },
